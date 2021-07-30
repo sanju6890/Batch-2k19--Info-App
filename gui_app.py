99c79404e199a82eadeb704cedcb8f89bb0eace6
Sanjay Kumar <sanjusaikap2001@gmail.com>
2021-07-30 21:17:37 +0530

    Removed old app script file & added new script file

---
+++ gui_app.py
@@ -0,0 +1,71 @@
+from tkinter import *
+from tkinter import messagebox
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="sanju@6890",
+    database="batch_2k19")
+
+root=Tk()
+root.title("Student Info")
+root.geometry("1920x1080")
+root.configure(bg='cyan2')
+
+def search_in_database(query):
+    mycursor = mydb.cursor()
+    mycursor.execute(query)
+    return mycursor.fetchall()
+    
+
+def find():
+    try:
+        list_box.delete(0,END)
+        key = box.get()
+        if(len(key) == 8):
+            query = f"Select * from students_info where roll_no = {int(key)}"
+            results = search_in_database(query)
+            if results == None:
+                messagebox.showwarning('Student Info','Record Not Found')
+                return
+            for result in results:
+                for item in result:
+                    list_box.insert(END, item)
+        else:
+            query = f"Select * from students_info where branch = '{key}'"
+            results = search_in_database(query)
+            if results == None:
+                messagebox.showwarning('Student Info','Record Not Found')
+                return
+            list_box.insert(END, f"Roll No   Name   E-mail ID")
+            for result in results:
+                data=f"{result[0]} {result[1].upper()} ({result[2]})"
+                list_box.insert(END,data)
+        
+        # messagebox.showinfo('Student Info',data)
+    except ValueError:
+        messagebox.showerror('Student Info','Incorrect Input')
+
+
+header = Label(root, text='NIT, Jalandhar B.Tech Batch 2k19',bg="cyan4",fg="white",font=("Times", "16", "bold italic"),borderwidth=5, relief=SUNKEN)
+header.pack(pady=5)
+
+input_frame=Frame(root, borderwidth = 5, bg = 'cyan4', relief = SUNKEN, height=20, width=30)
+input_frame.pack()
+
+label = Label(input_frame, text='Enter Roll No or Department',bg='black',fg='white',font=("Times", "14", "bold"),borderwidth=5, relief=SUNKEN)
+box = Entry(input_frame,font=("Times", "12", "bold"),borderwidth=5, width=30)
+button = Button(input_frame, text='Search',bg="black",fg="white",font=("Times", "12", "bold"),borderwidth=5,command=find)
+
+label.grid(row=0, column=1, padx=10)
+box.grid(row=0, column=2, padx=10)
+button.grid(row=0, column=3, padx=10)
+
+list_box= Listbox(root, bg="black", fg="yellow", font=("Times", "12"), borderwidth=5, height = 30 ,width = 175)
+list_box.pack(pady=10)
+
+label = Label(root, text='SANJAY KUMAR (C) 2020',bg='cyan2',font=("Times", "12", "bold"),borderwidth=5)
+label.pack(pady=5)
+
+root.mainloop()
