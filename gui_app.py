7aab4c7d2efad6d442b39ce9b15cf787d2a38d2b
Sanjay Kumar <sanjusaikap2001@gmail.com>
2021-07-30 21:40:38 +0530

    Cleaned code & added section comments

--- gui_app.py
+++ gui_app.py
@@ -5,7 +5,7 @@ import mysql.connector
 mydb = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="sanju@6890",
+    password="abcde@12345",
     database="batch_2k19")
 
 root=Tk()
@@ -20,51 +20,48 @@ def search_in_database(query):
     
 
 def find():
-    try:
-        list_box.delete(0,END)
-        key = box.get()
-        if(len(key) == 8):
+    list_box.delete(0,END)
+    key = box.get()
+    if(len(key) == 8):
+            # Search by Student's Roll NO. (Individual Records)
             query = f"Select * from students_info where roll_no = {int(key)}"
             results = search_in_database(query)
-            if results == None:
-                messagebox.showwarning('Student Info','Record Not Found')
-                return
             for result in results:
                 for item in result:
                     list_box.insert(END, item)
-        else:
-            query = f"Select * from students_info where branch = '{key}'"
-            results = search_in_database(query)
-            if results == None:
-                messagebox.showwarning('Student Info','Record Not Found')
-                return
-            list_box.insert(END, f"Roll No   Name   E-mail ID")
-            for result in results:
-                data=f"{result[0]} {result[1].upper()} ({result[2]})"
-                list_box.insert(END,data)
+    else:
+        # Search by Student's Department (Department-wise Students' Records)
+        query = f"Select * from students_info where branch = '{key}'"
+        results = search_in_database(query)
+        list_box.insert(END, f"Roll No   Name   E-mail ID")
+        for result in results:
+            data=f"{result[0]} {result[1].upper()} ({result[2]})"
+            list_box.insert(END,data)         
         
-        # messagebox.showinfo('Student Info',data)
-    except ValueError:
-        messagebox.showerror('Student Info','Incorrect Input')
 
-
+# App Header or Title
 header = Label(root, text='NIT, Jalandhar B.Tech Batch 2k19',bg="cyan4",fg="white",font=("Times", "16", "bold italic"),borderwidth=5, relief=SUNKEN)
 header.pack(pady=5)
 
+# Search bar & input frame
 input_frame=Frame(root, borderwidth = 5, bg = 'cyan4', relief = SUNKEN, height=20, width=30)
 input_frame.pack()
 
+# Search Label, Entry field, & Button
 label = Label(input_frame, text='Enter Roll No or Department',bg='black',fg='white',font=("Times", "14", "bold"),borderwidth=5, relief=SUNKEN)
 box = Entry(input_frame,font=("Times", "12", "bold"),borderwidth=5, width=30)
 button = Button(input_frame, text='Search',bg="black",fg="white",font=("Times", "12", "bold"),borderwidth=5,command=find)
 
+# Position all the elements in the input frame
 label.grid(row=0, column=1, padx=10)
 box.grid(row=0, column=2, padx=10)
 button.grid(row=0, column=3, padx=10)
 
+# List Box to display the search result
 list_box= Listbox(root, bg="black", fg="yellow", font=("Times", "12"), borderwidth=5, height = 30 ,width = 175)
 list_box.pack(pady=10)
 
+# Copy Right Label
 label = Label(root, text='SANJAY KUMAR (C) 2020',bg='cyan2',font=("Times", "12", "bold"),borderwidth=5)
 label.pack(pady=5)
 
